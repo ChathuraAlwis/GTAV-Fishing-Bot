90a043135652bdbcd54dbcf5875979db2f657f6d
Chathura Alwis <chathura7alwis@gmail.com>
2021-08-05 01:48:46 +0530

    moved code into windowCalpture class and used it

--- main.py
+++ main.py
@@ -1,44 +1,4 @@
-from getScreenShot import getScreenShot
-import pygetwindow
-import keyboard
-from time import time
-import cv2 as cv
-import numpy as np
+from windowCapture import windowCapture
 
-ss = getScreenShot()
-
-ss.setImgPath("C:\\Users\\chasa\\Desktop\\screenshot.png")
-
-
-loop_time = time()
-while True:
-
-    try:
-        ss.setTitle(pygetwindow.getActiveWindow().title)
-        ss.setBorders(pygetwindow.getWindowsWithTitle(ss.getTitle())[0])
-    except:
-        continue
-
-    try:
-        print('FPS {}'.format(1/(time() - loop_time)))
-    except:
-        pass
-    loop_time = time()
-
-    # if ss.getTitle() == 'Calculator':
-
-    res_image = ss.take()
-
-    res_image = np.array(res_image)
-    res_image = cv.cvtColor(res_image, cv.COLOR_RGB2BGR)
-    #---------OR--------
-    # res_image = cv.imread(ss.getPath())
-
-    # res_image.show(ss.getPath())
-    # breakq
-    cv.imshow('Computer Vision', res_image)
-    cv.waitKey(1)
-
-    if keyboard.is_pressed('q'):
-        cv.destroyAllWindows()
-        break
+window = windowCapture()
+window.capture()
