e9258b2467f8f3889d20edfaf5070054a894ff2c
Chathura Alwis <chathura7alwis@gmail.com>
2021-08-10 20:23:23 +0530

    update setBorders

--- getScreenShot.py
+++ getScreenShot.py
@@ -4,6 +4,7 @@ class getScreenShot:
 
     ImgPath = None
     Title = None
+    mid = None
     x1 = x2 = y1 = y2 = None
 
     def setImgPath(self, path):
@@ -12,17 +13,18 @@ class getScreenShot:
     def setTitle(self, title):
         self.Title = title
 
-    def setBorders(self, window):
-        try:
-            self.x1 = window.left
-            self.y1 = window.top
-            self.x2 = window.width + self.x1 
-            self.y2 = window.height + self.y1
-        except:
-            self.x1 = window[0]
-            self.y1 = window[1]
-            self.x2 = window[2]
-            self.y2 = window[3]
+    def setBorders(self, window, w, h, gap):
+        # try:
+        self.mid = (window.left + window.width/2, window.top + window.height/2)
+        self.x1 = self.mid[0] - w/2
+        self.y1 = self.mid[1] - h - gap
+        self.x2 = self.mid[0] + w/2
+        self.y2 = self.mid[1] - gap
+        # except:
+        #     self.x1 = window.left
+        #     self.y1 = window.top
+        #     self.x2 = window.width + self.x1 
+        #     self.y2 = window.height + self.y1
 
     def getPath(self):
         return self.ImgPath
