46753177af50b9ac0d055a34ae79486bf5c05c4f
Chathura Alwis <chathura7alwis@gmail.com>
2021-08-10 20:25:22 +0530

    create variables for capturing

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from windowCapture import windowCapture
 
+window_title = 'fishing.mp4'
+w, h, gap = 15, 145, 13
+percentage = 0.93
+delay = 0.3 * percentage # default value 0.3
+
 window = windowCapture()
-window.capture()
+window.capture(w, h, gap, delay, window_title)
