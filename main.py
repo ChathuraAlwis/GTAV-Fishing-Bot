08265e10ab7cbeb3285ec5fae24832436cf22b85
Chathura Alwis <chathura7alwis@gmail.com>
2021-08-10 20:55:31 +0530

    remove percentage variable

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from windowCapture import windowCapture
 
 window_title = 'fishing.mp4'
 w, h, gap = 15, 145, 13
-percentage = 0.93
-delay = 0.3 * percentage # default value 0.3
+delay = 0.3  # default value 0.3
 
 window = windowCapture()
 window.capture(w, h, gap, delay, window_title)
